c6f47859799038c32a1113b1e4cde63976a4edc6
Givememy2007 <kortin95@mail.ru>
2017-04-16 08:57:57 +0700

    Инициализация необходимых переменных и рандом от времени

--- Евгений/Choosing_word.cpp
+++ Евгений/Choosing_word.cpp
@@ -6,5 +6,12 @@ using namespace std;
 
 int main()
 {
-	
+	srand( time( 0 ) );
+	int NumberOfTheme;
+	string Word;
+	cin>>NumberOfTheme;
+	ifstream File;
+	int i = 0, Number;
+	Number = rand() % 10 + 1;
+	return 0;	
 }
