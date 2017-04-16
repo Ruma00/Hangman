41ba8159ad47347d141b6ba5528c88eff91e76cf
Givememy2007 <kortin95@mail.ru>
2017-04-16 09:00:10 +0700

    Оператор выбора для одного из 6 файлов

--- Евгений/Choosing_word.cpp
+++ Евгений/Choosing_word.cpp
@@ -13,5 +13,16 @@ int main()
 	ifstream File;
 	int i = 0, Number;
 	Number = rand() % 10 + 1;
+		
+	switch( NumberOfTheme )
+	{
+		case 1:
+		case 2:
+		case 3:
+		case 4:
+		case 5:
+		case 6:
+	}
+	
 	return 0;	
 }
