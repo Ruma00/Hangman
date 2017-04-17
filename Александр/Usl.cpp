6e9db69beb2bfa41e8510b968229a62f9e344581
Sahan <den2011sahan@mail.ru>
2017-04-17 09:44:01 +0700

    Завершённая функция setWord()

--- Александр/Usl.cpp
+++ Александр/Usl.cpp
@@ -7,5 +7,10 @@ static char * word2;
 static int x = 0;
 
 void setWord(char ch[]){
-	
+	word1 = ch;
+	word2 = new char;
+	for(int i = 0; i < 4; i++){
+		word2[i] = ' ';
+	//	printf("%c",word2[i]);
+	}
 }
