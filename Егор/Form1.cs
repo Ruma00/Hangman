35a6c8459be9cd3441c32edbc1f70c772a378866
Ruma00 <hackgen0@gmail.com>
2017-05-09 23:58:26 +0500

    Working buttons

--- Егор/Form1.cs
+++ Егор/Form1.cs
@@ -10,7 +10,9 @@ using System.Windows.Forms;
 
 namespace Hangman_release
 {
-    public Form1()
+    public partial class Form1 : Form
+    {
+        public Form1()
         {
             InitializeComponent();
             dataGridView1.CellBorderStyle = DataGridViewCellBorderStyle.Single;
@@ -24,8 +26,14 @@ namespace Hangman_release
             pictureBox1.Image = Image.FromFile(path + "Init.jpg");
             InitButton(false);
         }
-	
-	void SetButton(bool enabled)
+
+        Class1 a = new Class1();
+        Class2 b = new Class2();
+        int count_c = 0;
+        int count_i = 0;
+        string path = "Icons\\";
+
+        void SetButton(bool enabled)
         {
             for (int i = 2; i < 35; i++)
             {
@@ -86,8 +94,8 @@ namespace Hangman_release
             control.Enabled = false;
             dataGridView1.ClearSelection();
         }
-	
-	private void button1_Click(object sender, EventArgs e)
+
+        private void button1_Click(object sender, EventArgs e)
         {
             count_i = 4;
             dataGridView1.Columns.Clear();
@@ -104,169 +112,174 @@ namespace Hangman_release
             dataGridView1.ClearSelection();
             InitButton(true);
         }
-	
-	private void button2_Click(object sender, EventArgs e)
+
+        private void button2_Click(object sender, EventArgs e)
         {
-            
+            Check(button2, 'à');
         }
 
         private void button3_Click(object sender, EventArgs e)
         {
-            
+            Check(button3, 'á');
         }
 
         private void button4_Click(object sender, EventArgs e)
         {
-            
+            Check(button4, 'â');
         }
 
         private void button5_Click(object sender, EventArgs e)
         {
-            
+            Check(button5, 'ã');
         }
 
         private void button6_Click(object sender, EventArgs e)
         {
-            
+            Check(button6, 'ä');
         }
 
         private void button7_Click(object sender, EventArgs e)
         {
-            
+            Check(button7, 'å');
+            button8.Enabled = false;
+            button8.BackColor = button7.BackColor;
         }
 
         private void button8_Click(object sender, EventArgs e)
         {
-			
+            Check(button8, 'å');
+            button7.Enabled = false;
+            button7.BackColor = button8.BackColor;
         }
 
         private void button9_Click(object sender, EventArgs e)
         {
-            
+            Check(button9, 'æ');
         }
 
         private void button10_Click(object sender, EventArgs e)
         {
-            
+            Check(button10, 'ç');
         }
 
         private void button11_Click(object sender, EventArgs e)
         {
-            
+            Check(button11, 'è');
         }
 
         private void button12_Click(object sender, EventArgs e)
         {
-            
+            Check(button12, 'é');
         }
 
         private void button13_Click(object sender, EventArgs e)
         {
-            
+            Check(button13, 'ê');
         }
 
         private void button14_Click(object sender, EventArgs e)
         {
-            
+            Check(button14, 'ë');
         }
 
         private void button15_Click(object sender, EventArgs e)
         {
-            
+            Check(button15, 'ì');
         }
 
         private void button16_Click(object sender, EventArgs e)
         {
-            
+            Check(button16, 'í');
         }
 
         private void button17_Click(object sender, EventArgs e)
         {
-            
+            Check(button17, 'î');
         }
 
         private void button18_Click(object sender, EventArgs e)
         {
-            
+            Check(button18, 'ï');
         }
 
         private void button19_Click(object sender, EventArgs e)
         {
-            
+            Check(button19, 'ð');
         }
 
         private void button20_Click(object sender, EventArgs e)
         {
-            
+            Check(button20, 'ñ');
         }
 
         private void button21_Click(object sender, EventArgs e)
         {
-            
+            Check(button21, 'ò');
         }
 
         private void button22_Click(object sender, EventArgs e)
         {
-            
+            Check(button22, 'ó');
         }
 
         private void button23_Click(object sender, EventArgs e)
         {
-            
+            Check(button23, 'ô');
         }
 
         private void button24_Click(object sender, EventArgs e)
         {
-            
+            Check(button24, 'õ');
         }
 
         private void button25_Click(object sender, EventArgs e)
         {
-            
+            Check(button25, 'ö');
         }
 
         private void button26_Click(object sender, EventArgs e)
         {
-            
+            Check(button26, '÷');
         }
 
         private void button27_Click(object sender, EventArgs e)
         {
-            
+            Check(button26, 'ø');
         }
 
         private void button28_Click(object sender, EventArgs e)
         {
-            
+            Check(button28, 'ù');
         }
 
         private void button29_Click(object sender, EventArgs e)
         {
-            
+            Check(button29, 'ü');
         }
 
         private void button30_Click(object sender, EventArgs e)
         {
-            
+            Check(button30, 'û');
         }
 
         private void button31_Click(object sender, EventArgs e)
         {
-            
+            Check(button31, 'ú');
         }
 
         private void button32_Click(object sender, EventArgs e)
         {
-            
+            Check(button32, 'ý');
         }
 
         private void button33_Click(object sender, EventArgs e)
         {
-            
+            Check(button33, 'þ');
         }
 
         private void button34_Click(object sender, EventArgs e)
         {
-            
+            Check(button34, 'ÿ');
         }
+    }
 }
