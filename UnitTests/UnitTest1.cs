78c7e313e6b766341884350cb99cde87e4be6788
Ruma00 <hackgen0@gmail.com>
2017-05-27 01:52:14 +0500

    add empty unit tests

---
+++ UnitTests/UnitTest1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Hangman_release;
+using System.Linq;
+
+namespace UnitTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+        [TestMethod]
+        public void TestChoosing_word1()
+        {
+        }
+
+        [TestMethod]
+        public void TestChoosing_word2()
+        {
+        }
+
+        [TestMethod]
+        public void TestChoosing_word3()
+        {
+        }
+
+        [TestMethod]
+        public void TestChoosing_word4()
+        {
+        }
+
+        [TestMethod]
+        public void TestChoosing_word5()
+        {
+        }
+
+        [TestMethod]
+        public void TestSetWord1()
+        {
+        }
+
+        [TestMethod]
+        public void TestSetWord2()
+        {
+        }
+
+        [TestMethod]
+        public void TestGetWord2()
+        {
+        }
+    }
+}
